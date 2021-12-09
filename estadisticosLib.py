24d0168848b8a3dd8c0a9ae6c932377331df91ac
Santiago Medina <smbsanti2@gmail.com>
2021-12-09 16:32:45 +0100

    functions added to estadisticosLib.py

--- estadisticosLib.py
+++ estadisticosLib.py
@@ -1,1 +1,17 @@
-#Libreria para calcular estadisticos
+import pandas as pd
+from matplotlib import pyplot as plt
+import threading as th
+import numpy as np
+from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
+
+
+class Estadistico:
+
+  def __init__(self):
+    pass
+
+  def mediaMovilNueva(n, historico):
+    mm10 = historico.iloc[:,4].rolling(n, min_periods=1).mean()
+    return mm10
+  
+ 
